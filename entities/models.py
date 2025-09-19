1426e5ee85b65d041a610e24a7a47ab1ca1f5f31
Fxhat <make772@ukr.net>
2025-09-19 19:20:25 +0300

    create models

--- entities/models.py
+++ entities/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+import hashlib
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class EntityType(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Entity(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    display_name = models.CharField(max_length=255)
+    type = models.ForeignKey("EntityType", on_delete=models.PROTECT)
+    valid_from = models.DateTimeField(default=timezone.now)
+    valid_to = models.DateTimeField(null=True, blank=True)
+    is_current = models.BooleanField(default=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["uid", "is_current"])
+        ]
+
+    def __str__(self):
+        return self.display_name
+
+
+class EntityDetail(models.Model):
+    entity = models.ForeignKey("Entity", on_delete=models.CASCADE)
+    code = models.CharField(max_length=255)
+    value = models.TextField()
+    valid_from = models.DateTimeField(default=timezone.now)
+    valid_to = models.DateTimeField(null=True, blank=True)
+    is_current = models.BooleanField(default=True)
+    hash_diff = models.CharField(max_length=64, editable=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["entity", "code", "is_current"])
+        ]
+
+    def save(self, *args, **kwargs):
+        self.hash_diff = hashlib.sha256(str(self.value).encode()).hexdigest()
+        super().save(*args, **kwargs)
